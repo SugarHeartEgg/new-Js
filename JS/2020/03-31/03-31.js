adf88a85b77a78fef8f82d20009fa24baaaf28b8
SugarHeartEgg <1330489329@qq.com>
2020-03-31 21:27:02 +0800

    ...

---
+++ JS/2020/03-31/03-31.js
@@ -0,0 +1,22 @@
+let set = /\d{2,4}/
+
+
+let pattern = /java/i;
+let str = 'javaScript'
+
+console.log(pattern.exec(str));
+
+
+// 不可重复赋值  以及不能重新定义相同的变量
+const a = 3;
+a = 4
+console.log(a);
+
+
+// 只能在定义的层级进行使用 ，一旦在其他位置使用就报错
+let b = 1;
+
+
+
+
+
